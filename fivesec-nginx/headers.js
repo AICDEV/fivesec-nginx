0ba78ac4fe4637676056f8465f5e0d82398a94a2
Fivesec <accounts@fivesec.de>
2025-04-24 19:25:41 +0200

    Update headers.js

--- fivesec-nginx/headers.js
+++ fivesec-nginx/headers.js
@@ -1,8 +1,24 @@
-function headers_to_json(r) {
-    var headersArray = Object.keys(r.headersIn).map(function(name) {
-        return { name: name, value: r.headersIn[name] };
-    });
-    return JSON.stringify(headersArray);
+function escapeJsonString(str) {
+	return str
+		.replace(/\\/g, '\\\\')  // backslashes
+		.replace(/"/g, '\\"')    // double quotes
+		.replace(/\u0008/g, '\\b') // backspace
+		.replace(/\t/g, '\\t')   // tab
+		.replace(/\n/g, '\\n')   // newline
+		.replace(/\f/g, '\\f')   // form feed
+		.replace(/\r/g, '\\r')   // carriage return
+		.replace(/</g, '')       // optional: strip <
+		.replace(/>/g, '');      // optional: strip >
 }
+
+function headers_to_json(r) {
+	var headers = {};
+	for (var h in r.headersIn) {
+		headers[escapeJsonString(h)] = escapeJsonString(r.headersIn[h]);
+
+	}
+	return JSON.stringify(headers);
+}
+
 
 export default { headers_to_json };

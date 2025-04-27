83783e45f338aad39da3be6cdab226bb0a59fd91
Fivesec <accounts@fivesec.de>
2025-04-27 18:13:49 +0200

    Update headers.js
    Fix a possible escape bug in json parsing

--- fivesec-nginx/headers.js
+++ fivesec-nginx/headers.js
@@ -1,24 +1,15 @@
-function escapeJsonString(str) {
-	return str
-		.replace(/\\/g, '\\\\')  // backslashes
-		.replace(/"/g, '\\"')    // double quotes
-		.replace(/\u0008/g, '\\b') // backspace
-		.replace(/\t/g, '\\t')   // tab
-		.replace(/\n/g, '\\n')   // newline
-		.replace(/\f/g, '\\f')   // form feed
-		.replace(/\r/g, '\\r')   // carriage return
-		.replace(/</g, '')       // optional: strip <
-		.replace(/>/g, '');      // optional: strip >
-}
-
 function headers_to_json(r) {
-	var headers = {};
-	for (var h in r.headersIn) {
-		headers[escapeJsonString(h)] = escapeJsonString(r.headersIn[h]);
-
-	}
-	return JSON.stringify(headers);
+    var headers = {};
+    if (r.headersIn) {
+        for (var h in r.headersIn) {
+            var value = r.headersIn[h];
+            if (value === undefined || value === null) {
+                value = "";
+            }
+            headers[h] = value;
+        }
+    }
+    return JSON.stringify(headers);
 }
-
 
 export default { headers_to_json };
